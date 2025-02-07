e4772fded6db035b5c260ab20985a927e8404171
murayamatoru <murayama@bronze.ocn.ne.jp>
2025-02-07 17:41:07 +0900

    Initializrで作成した初期プロジェクトを登録

---
+++ learnspringsecurity/src/main/java/com/example/learnspringsecurity/LearnspringsecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.learnspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnspringsecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnspringsecurityApplication.class, args);
+	}
+
+}
